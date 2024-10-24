dc2d05aab8533d64201397b5d9e126661905f5c0
N0mbreP <109170634+N0mbreP@users.noreply.github.com>
2024-10-24 15:40:00 +0000

    comit 1

---
+++ xat/src/components/Missatge.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    userFill: String
+})
+const emits=defineEmits(['enviarMisatge'])
+const nouMisatge=ref('')
+const enviarMisatge=()=>{
+    emits('enviarMisatge', nouMisatge.value)
+}
+</script>
+
+<template>
+    <input type="text" v-model="nouMisatge" placeholder="Escriu un missatge">
+    <input type="button" value="Enviar missatge" @click="enviarMisatge">
+</template>
---
+++ xat/src/components/Xat.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import Missatge from './Missatge.vue';
+const user="Kameron"
+const llistaMissatges=ref([])
+const gestionarMisatge=(nouMisatge)=>{
+    llistaMissatges.value.push({
+        id:Date.now(),
+        usuari:user,
+        contingut:nouMisatge
+    })
+}
+
+</script>
+
+<template>
+    <main>
+        <div>
+            <h2>Xat entre users</h2>
+            <ul>
+                <li v-for="missatge in llistaMissatges" :key="missatge.id">{{ missatge.usuari }} : {{ missatge.contingut }}</li>
+            </ul>
+            <Missatge :userFill=user @enviarMisatge="gestionarMisatge"></Missatge>
+            
+        </div>
+    </main>
+
+
+</template>
